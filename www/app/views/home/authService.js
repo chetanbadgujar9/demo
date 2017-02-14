8be4935c1120e825fd6af183f5489937b56fd5ff
Chetan Badgujar <Chetan Badgujar>
2017-02-14 15:44:35 +0530

    Initial commit

---
+++ www/app/views/home/authService.js
@@ -0,0 +1,139 @@
+﻿'use strict';
+angular.module('starter').factory('authService', ['$http', function ($http) {
+
+    var landingPageSearchResultFactory = {};
+    var _login = function (searchQuery) {
+	 
+        return $http.post('http://localhost:57585/api/User/Save', searchQuery).then(function (results) {
+            return results;
+        });
+    }
+    //var _getStates = function () {
+    //    return $http.get('../api/GlobalProvider/SearchCity').then(function (results) {
+    //        return results;
+    //    });
+    //}
+    //var _getSpeciality = function () {
+    //    return $http.get('/api/GlobalProvider/SearchSpecialization').then(function (results) {
+    //        return results;
+    //    });
+    //}
+    //var _getlocation = function () {
+    //    return $http.get('/api/GlobalProvider/SearchLocation').then(function (results) {
+    //        return results;
+    //    });
+    //}
+    //var _searchWithFilter = function (filter) {
+    //    return $http.post('../api/GlobalProvider/Search', filter).then(function (results) {
+    //        return results;
+    //    });
+    //}
+    //var _getCities = function () {
+    //    return $http.get('../api/City/Search').then(function (results) {
+    //        return results;
+    //    });
+    //}
+    //var _getLoggedInPatientCity = function () {
+    //    var model = {}
+    //    model.ID = 0;
+    //    return $http.post('../api/patient/Edit', model).then(function (results) {
+    //        return results;
+    //    });
+    //}
+    //var _getDiagnosticServices = function () {
+    //    return $http.get('../api/Suggestion/SearchDiagnosticKeyword?SearchFor=DiagnosticCenterKeyword&&SearchString=').then(function (results) {
+    //        return results;
+    //    });
+    //}
+    //var _getDiagnosticlocation = function () {
+    //    return $http.get('/api/GlobalServiceProvider/SearchLocation').then(function (results) {
+    //        return results;
+    //    });
+    //}
+    //var _getDiagnosticCity = function () {
+    //    return $http.get('/api/GlobalServiceProvider/SearchCity').then(function (results) {
+    //        return results;
+    //    });
+    //}
+    //var _searchDiagnosticQueryResult = function (searchQuery) {
+    //    return $http.post('api/GlobalServiceProvider/Search', searchQuery).then(function (results) {
+    //        return results;
+    //    });
+    //}
+
+
+    //var _saveLikeDoctor = function (searchQuery) {
+    //    return $http.post('api/PatientFavorites/SaveLikeDoctor', searchQuery).then(function (results) {
+    //        return results;
+    //    });
+    //}
+
+    //var _saveFavoriteDoctor = function (searchQuery) {
+    //    return $http.post('api/PatientFavorites/SaveFavoriteDoctor', searchQuery).then(function (results) {
+    //        return results;
+    //    });
+    //}
+
+    //var _saveLikeDiagnisticCenter = function (searchQuery) {
+    //    return $http.post('api/PatientFavorites/SaveLikeDiagnisticCenter', searchQuery).then(function (results) {
+    //        return results;
+    //    });
+    //}
+
+    //var _saveFavoriteDiagnisticCenter = function (searchQuery) {
+    //    return $http.post('api/PatientFavorites/SaveFavoriteDiagnisticCenter', searchQuery).then(function (results) {
+    //        return results;
+    //    });
+    //}
+
+    //var _saveLikeMedicalCenter = function (searchQuery) {
+    //    return $http.post('api/PatientFavorites/SaveLikeMedicalStore', searchQuery).then(function (results) {
+    //        return results;
+    //    });
+    //}
+
+    //var _saveFavoriteMedicalCenter = function (searchQuery) {
+    //    return $http.post('api/PatientFavorites/SaveFavoriteMedicalStore', searchQuery).then(function (results) {
+    //        return results;
+    //    });
+    //}
+
+    //var _postComment = function (model) {
+    //    return $http.post('../api/Feedback/Save', model).then(function (results) {
+    //        return results;
+    //    });
+    //}
+    //var _getReviewResult = function (model) {
+    //    return $http.post('api/Feedback/GetReviewsByDoctor', model).then(function (results) {
+    //        return results;
+    //    });
+    //}
+    //var _requestAppointmentMessage = function (model) {
+    //    return $http.post('api/Appointment/RequestAppointment', model).then(function (results) {
+    //        return results;
+    //    });
+    //}
+
+    //landingPageSearchResultFactory.searchDiagnosticQueryResult = _searchDiagnosticQueryResult;
+    //landingPageSearchResultFactory.getDiagnosticCity = _getDiagnosticCity;
+    //landingPageSearchResultFactory.getDiagnosticlocation = _getDiagnosticlocation;
+    //landingPageSearchResultFactory.getDiagnosticServices = _getDiagnosticServices;
+    //landingPageSearchResultFactory.getlocation = _getlocation;
+    //landingPageSearchResultFactory.getSpeciality = _getSpeciality;
+    //landingPageSearchResultFactory.searchWithFilter = _searchWithFilter;
+    landingPageSearchResultFactory.login = _login;
+    //landingPageSearchResultFactory.getStates = _getStates;
+    //landingPageSearchResultFactory.getCities = _getCities;
+    //landingPageSearchResultFactory.getLoggedInPatientCity = _getLoggedInPatientCity;
+    //landingPageSearchResultFactory.postComment = _postComment;
+    //landingPageSearchResultFactory.saveLikeDoctor = _saveLikeDoctor;
+    //landingPageSearchResultFactory.saveFavoriteDoctor = _saveFavoriteDoctor;
+    //landingPageSearchResultFactory.saveLikeDiagnisticCenter = _saveLikeDiagnisticCenter
+    //landingPageSearchResultFactory.saveFavoriteDiagnisticCenter = _saveFavoriteDiagnisticCenter
+    //landingPageSearchResultFactory.saveLikeMedicalCenter = _saveLikeMedicalCenter
+    //landingPageSearchResultFactory.saveFavoriteMedicalCenter = _saveFavoriteMedicalCenter
+    //landingPageSearchResultFactory.getReviewResult = _getReviewResult;
+    //landingPageSearchResultFactory.requestAppointmentMessage = _requestAppointmentMessage;
+    return landingPageSearchResultFactory;
+}]);
+
---
+++ www/app/views/home/landingPageCtrl.js
@@ -0,0 +1,128 @@
+﻿(function () {
+    'use strict';
+    angular.module('starter').controller('landingPageCtrl', ['authService','$state', loginController]);
+    function loginController(authService,$state) {
+        var vm = this;
+        vm.message = '';
+        vm.loginData = {
+            userName: "",
+            password: ""
+        };
+		
+        vm.login = function () {
+            console.log('hio');
+            //angular.element('#loginPwd').blur();
+            //vm.message = '';
+            //if (vm.loginData.userName == "" || vm.loginData.userName == undefined) {
+            //    vm.message = "Please enter user name.";
+            //    common.showErrorPopup(vm.message);
+            //    return;
+            //}
+
+            //if (vm.loginData.password == "" || vm.loginData.password == undefined) {
+            //    vm.message = "Please enter password.";
+            //    common.showErrorPopup(vm.message);
+            //    return;
+            //}
+            //vm.loginFlag = false;
+            //var loggedInUser = {};
+            //vm.lastLoggedIn = '';
+            //vm.authentication = authService.authentication;
+            ////loggedInUser = localStorageService.get('loggedInUserName');
+
+            //common.showLoader();
+            vm.loginData = { username: 'chetan', password: 'amol' };
+            authService.login(vm.loginData).then(function (response) {
+			
+			console.log(response);
+            })
+			$state.go('login');
+            //    appHomeService.getCurrentLoggedInProvider().then(function (results) {
+            //        //if (!results.data.success && results.data.success != undefined) {
+            //        //    vm.errors = [];
+            //        //    vm.errors.push(common.getAjaxStateErrors(results)[0]);
+            //        //}
+            //        //else {
+            //        localStorageService.set('currentLoggedInProviderId', { currentLoggedInProviderId: results.data });
+            //        localStorageService.set('loggedInUserName', vm.authentication);
+            //        $rootScope.$broadcast('OnCurrentUserLoggedIn', { currentLoggedInProvider: localStorageService.get('currentLoggedInProviderId') });
+            //        $rootScope.$broadcast('OnSetLoggedInUserName', {});
+            //        var model = {};
+            //        model.forAppointment = "ForAppointmentCalendar";
+            //        appHomeService.getClinic(model).then(function (results) {
+            //            localStorageService.set('clinicId', { clinicId: results.data.items[0] });
+
+            //        }, function (error) {
+            //            common.hideLoader();
+            //        });
+
+            //        var currentUserType = localStorageService.get('currentLoggedInProviderId').currentLoggedInProviderId[1];
+            //        if (currentUserType == '2') {
+            //            var currentLoggedInUserInfo = localStorageService.get('currentLoggedInProviderId');
+            //            if (currentLoggedInUserInfo != null) {
+            //                if (currentLoggedInUserInfo.currentLoggedInProviderId.length > 0) {
+            //                    vm.lastLoggedIn = currentLoggedInUserInfo.currentLoggedInProviderId[2];
+            //                }
+            //            }
+            //            if (vm.lastLoggedIn == "") {
+            //                common.hideLoader();
+            //                vm.loginFlag = true;
+            //                localStorageService.set('isOTP', { status: true });
+            //                $state.go('app.doctorChangePassword');
+            //            }
+            //            else {
+            //                //$location.path('/app/appointment');
+            //                $state.go('app.doctorAppointment', {}, { reload: true });
+
+            //            }
+            //        }
+            //        else if (currentUserType == '3') {
+            //            patientService.getPatientByRegistrationId().then(function (results) {
+            //                if (!results.data.success && results.data.success != undefined) {
+            //                    //vm.errors = common.getAjaxStateErrors(results);
+            //                }
+            //                else {
+            //                    if (results.data.items != null)
+            //                        localStorageService.set('loggedInPatientId', results.data.items[0].id);
+            //                }
+
+            //            }, function (error) {
+            //                common.hideLoader();
+            //            });
+            //            var currentLoggedInUserInfo = localStorageService.get('currentLoggedInProviderId');
+            //            if (currentLoggedInUserInfo != null) {
+            //                if (currentLoggedInUserInfo.currentLoggedInProviderId.length > 0) {
+            //                    vm.lastLoggedIn = currentLoggedInUserInfo.currentLoggedInProviderId[2];
+            //                }
+            //            }
+            //            if (vm.lastLoggedIn == "") {
+            //                common.hideLoader();
+            //                vm.loginFlag = true;
+            //                localStorageService.set('isOTP', { status: true });
+            //                $state.go('app.changePassword');
+            //            }
+            //            else {
+            //                $state.go('app.searchDoctor');
+            //            }
+            //        }
+            //        appInitializer.Initialize();
+            //        $rootScope.$broadcast('LogOnStatuChanged', {});
+            //        common.hideLoader();
+            //        //$rootScope.$broadcast('onLoggedInSelectCity', {});
+            //        //}
+            //    }, function (error) {
+            //        common.hideLoader();
+            //    });
+
+            //},
+            //    function (err) {
+            //        common.hideLoader();
+            //        vm.message = err.error_description;
+            //        common.showErrorPopup(vm.message);
+            //        vm.loginData.password = "";
+            //    });
+        };
+
+      
+    };
+})();
